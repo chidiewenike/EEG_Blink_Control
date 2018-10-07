d94192eb5a50d91f8f32d4aab9e7a3a75eae4d26
Chidi Ewenike <chidiewenike@gmail.com>
2018-10-06 22:57:16 -0700

    Update Headset_demo.py

--- Headset_demo.py
+++ Headset_demo.py
@@ -12,11 +12,12 @@ servo = Servo(3)
 link = "http://192.168.1.85:1337/"
 f = urllib.request.urlopen(link)
 servo.write(0)
+
 while(True):
     f = urllib.request.urlopen(link)
     myfile = f.read()
     full_rot_bool = True
-    print (myfile)
+    
     if (full_rot_bool):
         servo.write(180)
         full_rot_bool = False
